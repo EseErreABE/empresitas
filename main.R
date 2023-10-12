f22f49df71c381192137f917f7a8c5c4fbe443a3
ABE <bravegg.andres@gmail.com>
2023-10-12 11:32:14 -0500

    Se crea main

---
+++ main.R
@@ -0,0 +1,17 @@
+source(file = "src/functions/suma.R")
+source(file = "src/functions/mult.R")
+
+res.mult = mult(3,2)
+res.suma = suma(3,3)
+
+res.mult == res.suma
+
+res.mult = mult(3,3)
+res.suma = suma(suma(3,3),3)
+
+res.mult == res.suma
+
+res.mult = mult(2,2)
+res.suma = suma(2,2)
+
+res.mult == res.suma
